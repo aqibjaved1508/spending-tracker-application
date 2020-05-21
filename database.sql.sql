fbb9f63153dfdc1b16b0566357d855f35c0699a3
Mohd Aqib Javed <aqibjaved1508@gmail.com>
2020-05-21 21:06:41 +0530

    project initialized

---
+++ database.sql.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE databasename;
+--
+-- Table structure for table `spendings`
+--
+CREATE TABLE `category_info` (
+  `category` varchar(50) NOT NULL,
+  PRIMARY KEY (`category`)
+)
+
+DROP TABLE IF EXISTS `spendings`;
+CREATE TABLE `spendings` (
+  `sid` int(11) NOT NULL AUTO_INCREMENT,
+  `category` varchar(50) NOT NULL,
+  `sdate` date NOT NULL,
+  `amount` int(11) NOT NULL,
+  PRIMARY KEY (`sid`)
+)
